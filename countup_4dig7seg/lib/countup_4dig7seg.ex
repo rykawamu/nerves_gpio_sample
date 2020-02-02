10668c988226c6f26c174df123f4e857f96ef95b
rykawamu <ryu2.endless.waltz@gmail.com>
2020-02-02 20:01:10 +0900

    init commit: countup 4digit 7segment led project.

---
+++ countup_4dig7seg/lib/countup_4dig7seg.ex
@@ -0,0 +1,18 @@
+defmodule Countup4dig7seg do
+  @moduledoc """
+  Documentation for Countup4dig7seg.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Countup4dig7seg.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ countup_4dig7seg/test/countup_4dig7seg_test.exs
@@ -0,0 +1,8 @@
+defmodule Countup4dig7segTest do
+  use ExUnit.Case
+  doctest Countup4dig7seg
+
+  test "greets the world" do
+    assert Countup4dig7seg.hello() == :world
+  end
+end
